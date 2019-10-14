dc770f69eee9db162720c67eccc380a401b29c76
utkarsh867 <utkarsh867@gmail.com>
2019-10-15 05:15:10 +0800

    Updates

---
+++ precache-manifest.48979faa698cdd32fe6c102ac1c3e389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999487c80b44b24d28271c6c119843c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0213e09f48e25e303d07",
+    "url": "/static/js/2.c350014a.chunk.js"
+  },
+  {
+    "revision": "72c92c4fd5b24fa7699f",
+    "url": "/static/js/main.6f31e594.chunk.js"
+  },
+  {
+    "revision": "09668c278f0c2c0f9cc8",
+    "url": "/static/js/runtime-main.cef8152d.js"
+  },
+  {
+    "revision": "b933b549e831fc0bf28b38e63fa08a61",
+    "url": "/static/media/qrcode.b933b549.jpg"
+  }
+]);
